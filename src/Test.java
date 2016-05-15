c260a208ab67690f6f4bc39429bd00c4a0d98065
solfaz <soltaninejad313@gmail.com>
2016-05-15 14:58:30 +0430

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -21,4 +21,55 @@ class Test
             System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
         }
     }
+    
+     private static void testsubtract()
+    {
+        int a = 7;
+        int b = 6;
+        int expected = 1;
+        BasicMath bm = new BasicMath();
+        int result = bm. subtract(a, b);
+        if (result == expected)
+        {
+            System.out.println(" subtract: OK");
+        }
+        else
+        {
+            System.out.println(" subtract: Failed, expected = " + expected + ", result = " + result);
+        }
+    }
+    
+     private static void testmultiply()
+    {
+        int a = 5;
+        int b = 6;
+        int expected =30;
+        BasicMath bm = new BasicMath();
+        int result = bm. multiply(a, b);
+        if (result == expected)
+        {
+            System.out.println(" multiply: OK");
+        }
+        else
+        {
+            System.out.println(" multiply: Failed, expected = " + expected + ", result = " + result);
+        }
+    }
+    
+     private static void testdivide()
+    {
+        int a = 30;
+        int b = 6;
+        int expected =5;
+        BasicMath bm = new BasicMath();
+        int result = bm. divide(a, b);
+        if (result == expected)
+        {
+            System.out.println(" divide: OK");
+        }
+        else
+        {
+            System.out.println(" divide: Failed, expected = " + expected + ", result = " + result);
+        }
+    }
 }
